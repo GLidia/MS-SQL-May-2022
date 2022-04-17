2d4a53aca5276ed123a17b69207de9a9a733e905
GLidia <74815962+GLidia@users.noreply.github.com>
2022-04-17 11:17:39 +0300

    Added problems 1-2 and the problem descriptions

---
+++ 03_Table Relations/01_OneToOneRelationship.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Persons (
+	PersonID INT IDENTITY,
+	FirstName VARCHAR(20) NOT NULL,
+	Salary DECIMAL(9,2),
+	PassportID INT NOT NULL
+)
+
+INSERT INTO Persons
+VALUES
+	('Roberto', 43300.00, 102),
+	('Tom', 56100.00, 103),
+	('Yana', 60200.00, 101)
+
+CREATE TABLE Passports (
+	PassportID INT PRIMARY KEY,
+	PassportNumber VARCHAR(20) NOT NULL
+)
+
+INSERT INTO Passports
+VALUES
+	(101, 'N34FG21B'),
+	(102, 'K65LO4R7'),
+	(103, 'ZE657QP2')
+
+ALTER TABLE Persons
+ADD PRIMARY KEY(PersonID)
+
+ALTER TABLE Persons
+ADD FOREIGN KEY (PassportID) REFERENCES Passports(PassportID)
